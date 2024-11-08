31dc864716691fcca8b21c21d6666381e75812b2
오세민 <oh.semin@cashwalk.io>
2024-11-08 12:28:17 +0900

    add(BaseActivity): add BaseActivity and fix MainActivity

---
+++ app/src/main/java/com/semin/androidarchitectureexample/presentation/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.semin.androidarchitectureexample.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+/**
+ * Activity for ViewDataBinding
+ */
+abstract class BaseActivity<Binding: ViewDataBinding>(@LayoutRes val layoutId: Int): AppCompatActivity() {
+    private var binding: Binding? = null
+
+    protected fun bind(block: Binding.() -> Unit) {
+        binding?.let(block)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding?.initView()
+    }
+
+    abstract fun Binding.initView()
+}
--- app/src/main/java/com/semin/androidarchitectureexample/presentation/main/MainActivity.kt
+++ app/src/main/java/com/semin/androidarchitectureexample/presentation/main/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.semin.androidarchitectureexample.presentation.main
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.semin.androidarchitectureexample.R
+import com.semin.androidarchitectureexample.databinding.ActivityMainBinding
+import com.semin.androidarchitectureexample.presentation.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>(layoutId = R.layout.activity_main) {
+
+    override fun ActivityMainBinding.initView() {
+
     }
 }
