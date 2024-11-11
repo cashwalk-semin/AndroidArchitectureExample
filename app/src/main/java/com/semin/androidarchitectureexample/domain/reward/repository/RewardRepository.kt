810fd8a16193380a8c81f43c754913fb75dc70d9
오세민 <oh.semin@cashwalk.io>
2024-11-11 12:29:05 +0900

    fix(Repository): add annotation

--- app/src/main/java/com/semin/androidarchitectureexample/domain/reward/repository/RewardRepository.kt
+++ app/src/main/java/com/semin/androidarchitectureexample/domain/reward/repository/RewardRepository.kt
@@ -3,6 +3,12 @@ package com.semin.androidarchitectureexample.domain.reward.repository
 import com.semin.androidarchitectureexample.domain.reward.model.Reward
 import kotlinx.coroutines.flow.Flow
 
+/**
+ *  반환형이 Flow<*>인 경우는 suspend를 명시하지 않음.
+ *  Flow의 collect가 suspend이기 때문
+ *
+ *  그 외 update, delete, insert는 suspend를 명시
+ */
 interface RewardRepository {
     fun getReward(): Flow<Reward>
     suspend fun updateName(name: String)
--- app/src/main/java/com/semin/androidarchitectureexample/domain/user/repository/UserRepository.kt
+++ app/src/main/java/com/semin/androidarchitectureexample/domain/user/repository/UserRepository.kt
@@ -3,6 +3,12 @@ package com.semin.androidarchitectureexample.domain.user.repository
 import com.semin.androidarchitectureexample.domain.reward.model.User
 import kotlinx.coroutines.flow.Flow
 
+/**
+ *  반환형이 Flow<*>인 경우는 suspend를 명시하지 않음.
+ *  Flow의 collect가 suspend이기 때문
+ *
+ *  그 외 update, delete, insert는 suspend를 명시
+ */
 interface UserRepository {
     fun getUser(): Flow<User>
     suspend fun updateName(name: String)
